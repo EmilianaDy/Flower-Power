aa26e0e4793cc5ee6baf8f061c0a6e96fa1a47cc
Emiliana <emiliana@lunarlogic.io>
2016-06-13 15:40:47 +0200

    Add responsive header

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 $(function(){
  var shrinkHeader = 3;
+      $window = $(window)
   $(window).scroll(function() {
     var scroll = getCurrentScroll();
-      if ( scroll >= shrinkHeader ) {
+      if ( scroll >= shrinkHeader && $window.width() > 947) {
            $('header').addClass('shrink');
         }
         else {
             $('header').removeClass('shrink');
         }
   });
+
 function getCurrentScroll() {
     return window.pageYOffset;
     }
 });
+
